4493c7a03220f252e3af560e76e7bfb919211add
CeciliaL1 <cecilia.lepik@hotmail.com>
2022-12-25 19:35:53 +0100

    if applides tuhis will ad comments

--- src/convertTime.ts
+++ src/convertTime.ts
@@ -1,13 +1,23 @@
+/**
+ * CONVERTS TIME FOR CURRENTTIME.
+ */
+
 export function convertTime(timestamp: any) {
   const time = (new Date(timestamp)).toTimeString();
   return time.replace(':00 GMT+0100 (centraleuropeisk normaltid)', '');
 }
 
+/**
+ *  CONVERTING FORECAST TIME 
+ * @param validtime 
+ * @returns  stamp as example : 2022-12-25T22:00:00Z
+ * 
+ * firstReg matching = 2022-12-25T'
+ * secondReg matching = :00Z
+ */
 
-//NOT DONE!
 export function convertForeCastTime(validtime: any){
 const foreCastTime = validtime;
-// creating regwxp
 const firstReg = new RegExp(/\d{4}\-\d{2}\-\d{2}\w/g);
 const secondReg = new RegExp(/\:\d{2}\w/g);
 // replacing first section of regex and saving in a variable
