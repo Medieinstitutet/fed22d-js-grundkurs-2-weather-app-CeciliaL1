d76ba0e6c7656a4963ec7d7a29afb6677986e06e
CeciliaL1 <cecilia.lepik@hotmail.com>
2022-12-25 19:38:09 +0100

    if applied this will add comments

--- src/convertTime.ts
+++ src/convertTime.ts
@@ -11,20 +11,21 @@ export function convertTime(timestamp: any) {
  *  CONVERTING FORECAST TIME 
  * @param validtime 
  * @returns  stamp as example : 2022-12-25T22:00:00Z
- * 
- * firstReg matching = 2022-12-25T'
- * secondReg matching = :00Z
  */
 
 export function convertForeCastTime(validtime: any){
-const foreCastTime = validtime;
-const firstReg = new RegExp(/\d{4}\-\d{2}\-\d{2}\w/g);
-const secondReg = new RegExp(/\:\d{2}\w/g);
-// replacing first section of regex and saving in a variable
-const  newForeCast = foreCastTime.replace(firstReg, '');
-// taking the new variable and replacing second regex and creating a new variable to return
-const secondNewForeCast = newForeCast.replace(secondReg, '');
-return secondNewForeCast;
+  const foreCastTime = validtime;
 
+  // firstReg matching = 2022-12-25T
+  const firstReg = new RegExp(/\d{4}\-\d{2}\-\d{2}\w/g);
+  //secondReg matching = :00Z
+  const secondReg = new RegExp(/\:\d{2}\w/g);
+
+  // replacing first section of regex and saving in a variable
+  const  newForeCast = foreCastTime.replace(firstReg, '');
+  // taking the new variable and replacing second regex and creating a new variable to return
+  const secondNewForeCast = newForeCast.replace(secondReg, '');
+
+return secondNewForeCast;
 }
 
