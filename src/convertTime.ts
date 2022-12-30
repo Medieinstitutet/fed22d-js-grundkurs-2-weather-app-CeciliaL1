72a2c7518ba84252b3d511b457caffb1524223dd
CeciliaL1 <cecilia.lepik@hotmail.com>
2022-12-30 19:28:49 +0100

    if applied this will do small changes

--- src/convertTime.ts
+++ src/convertTime.ts
@@ -4,7 +4,7 @@
 
 export function convertTime(timestamp: any) {
   const time = (new Date(timestamp)).toTimeString();
-  return time.replace(':00 GMT+0100 (centraleuropeisk normaltid)', '');
+  return time.replace(':00 GMT+0100 (centraleuropeisk normaltid)', ' ');
 }
 
 /**
